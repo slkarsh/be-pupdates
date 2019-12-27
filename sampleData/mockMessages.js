faf50e86069627e0f996c7e6523f136b53de283d
Sara <saralkarsh@gmail.com>
2019-12-27 15:27:11 -0700

    Fix content column for messages sample data

--- sampleData/mockMessages.js
+++ sampleData/mockMessages.js
@@ -2,16 +2,16 @@ module.exports = [
   {
     id: 1,
     user_id: 1,
-    description: 'Hello I like dogs'
+    content: 'Hello I like dogs'
   },
   {
     id: 2,
     user_id: 2,
-    descrption: 'Yes dogs are great'
+    content: 'Yes dogs are great'
   },
   {
     id: 3,
     user_id: 1,
-    descrption: 'Shall our dogs meet?'
+    content: 'Shall our dogs meet?'
   }
 ]
