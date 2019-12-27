8c6d544c44a791261219e26ea3343fb8a45f2853
Sara <saralkarsh@gmail.com>
2019-12-27 14:40:46 -0700

    Add sample messages data

---
+++ sampleData/mockMessages.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    user_id: 1,
+    description: 'Hello I like dogs'
+  },
+  {
+    id: 2,
+    user_id: 2,
+    descrption: 'Yes dogs are great'
+  },
+  {
+    id: 3,
+    user_id: 1,
+    descrption: 'Shall our dogs meet?'
+  }
+]
