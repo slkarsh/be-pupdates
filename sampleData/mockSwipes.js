6ea5686396158964533f778134b09b5a992bdf6f
Sara <saralkarsh@gmail.com>
2019-12-27 14:42:29 -0700

    Make sample swipes data

---
+++ sampleData/mockSwipes.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    user_id: 1,
+    shown_user_id: 4,
+    liked: true
+  },
+  {
+    id: 2,
+    user_id: 4,
+    shown_user_id: 1,
+    liked: true
+  },
+  {
+    id: 3,
+    user_id: 4,
+    shown_user_id: 2,
+    liked: true
+  },
+  {
+    id: 4,
+    user_id: 2,
+    shown_user_id: 4,
+    liked: false
+  }
+]
