fc5539c0e46313c37dd98276647a7208e9216a21
Sara <saralkarsh@gmail.com>
2019-12-27 14:37:30 -0700

    Add dog images sample data

---
+++ sampleData/mockDogImages.js
@@ -0,0 +1,77 @@
+module.exports = [
+  {
+    id: 1,
+    dog_id: 1,
+    image_url: 'https://images.pexels.com/photos/752383/pexels-photo-752383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 2,
+    dog_id: 1,
+    image_url: 'https://images.pexels.com/photos/1959055/pexels-photo-1959055.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 3,
+    dog_id: 2,
+    image_url: 'https://images.pexels.com/photos/1938125/pexels-photo-1938125.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 4,
+    dog_id: 3,
+    image_url: 'https://images.pexels.com/photos/1242419/pexels-photo-1242419.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 5,
+    dog_id: 3,
+    image_url: 'https://images.pexels.com/photos/89781/doberman-dog-animal-world-animal-89781.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 6,
+    dog_id: 4,
+    image_url: 'https://images.pexels.com/photos/1490908/pexels-photo-1490908.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 7,
+    dog_id: 4,
+    image_url: 'https://images.pexels.com/photos/92380/pexels-photo-92380.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 8,
+    dog_id: 4,
+    image_url: 'https://images.pexels.com/photos/1431762/pexels-photo-1431762.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 9,
+    dog_id: 4,
+    image_url: 'https://images.pexels.com/photos/54199/pexels-photo-54199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 10,
+    dog_id: 5,
+    image_url: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12230042/Papillon-On-White-01.jpg'
+  },
+  {
+    id: 11,
+    dog_id: 5,
+    image_url: 'http://static.ddmcdn.com/en-us/apl/breedselector/images/breed-selector/dogs/breeds/papillon_01_lg.jpg'
+  },
+  {
+    id: 12,
+    dog_id: 6,
+    image_url: 'https://www.purina.co.uk/sites/g/files/mcldtz2481/files/styles/nppe_breed_selector_500/public/breed_library/chihuahua_smooth_coat.jpg?itok=mvcwMTFj'
+  }, 
+  {
+    id: 13,
+    dog_id: 7,
+    image_url: 'https://vetstreet.brightspotcdn.com/dims4/default/1d87d20/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F3a%2Fc3%2F424ee1bf4768973966bde73acda8%2Fgerman-shepherd-AP-1S7FRX-645sm12913.jpg'
+  },
+  {
+    id: 14,
+    dog_id: 8,
+    image_url: 'https://www.allthingsdogs.com/wp-content/uploads/2019/07/Chocolate-Lab-Names-Feature.jpg'
+  }, 
+  {
+    id: 15,
+    dog_id: 9,
+    image_url: 'https://www.thesprucepets.com/thmb/6OX0VRYRfXclaryFZUeR-5j-7YA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/Stocksy_txpf4e26be2n2x100_Medium_774343-5afafb60c064710036564027.jpg'
+  }
+]
